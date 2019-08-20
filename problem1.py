060661a2856e823a08aa476e6d5ed0b221754084
SibiSagar <42875235+SibiSagar@users.noreply.github.com>
2019-08-20 21:27:29 +0530

    new guvi1

--- problem1.py
+++ problem1.py
@@ -1,8 +1,28 @@
-num=input()
-num=int(num)
-if num>0:
-    print("Positive")
-elif num<0:
-    print("Negative")
-elif num==0:
-    print("Zero")
+'''Given a string S of length N, print the vowels first and then consonants in the same order as they have occurred in the string.
+Input Size : N <= 10000
+Sample Testcase :
+INPUT
+asdaqrew
+OUTPUT
+aaesdqrw'''
+
+string=input()
+v=[]
+c=[]
+ostring=""
+vowel="aeiou"
+
+for i in range(0,len(string)):
+    if string[i] in vowel:
+        v.append(string[i])
+    else:
+    	c.append(string[i])
+
+v=sorted(v)
+c=sorted(c)
+v.extend(c)
+
+for j in v:
+	ostring+=j
+
+print(ostring)
